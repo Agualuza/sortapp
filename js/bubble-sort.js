df81f8d1ea69f2da2eea8f50c6de6c4bdb3296f4
Agualuza <iagoagualuza@id.uff.br>
2020-12-07 19:53:00 -0200

    Corrige bug do pause no bubble sort

--- js/bubble-sort.js
+++ js/bubble-sort.js
@@ -1,18 +1,18 @@
 const container = document.querySelector("#container-bubble-sort");
 
-async function bubbleSort(delay = 1000,l_i = 0,l_j = 0) {
+async function bubbleSort(delay = 1000,l_i = 0,l_j = 0, paused = false) {
     if (delay && typeof delay !== "number") {
       alert("Primeiro argumento tem que ser do tipo número");
       return;
     }
-    if(l_i > 0 || l_j > 0){
-      document.querySelectorAll(".block").forEach((e) => {
-        e.backgroundColor = "";
-      });
-    }
+   
     let blocks = document.querySelectorAll(".block");
-    for (let i = l_i; i < blocks.length - 1; i += 1) {
-      for (let j = l_j; j < blocks.length - i - 1; j += 1) {
+    for (let i = 0; i < blocks.length - 1; i += 1) {
+      console.log("i => "+i);
+      for (let j = 0; j < blocks.length - i - 1; j += 1) {
+        console.log("j => "+j);
+
+        console.log("locked => "+locked);
         if(locked) {
           var html = "<div class='clear-msg-change' align='center'><p style='font-size:42px;color:white;'><b>Pausado</b></p><div>"; 
           $('.clear-msg-change').remove();
@@ -21,28 +21,30 @@ async function bubbleSort(delay = 1000,l_i = 0,l_j = 0) {
         } else {
             locked_i = i;
             locked_j = j;
-            blocks[j].style.backgroundColor = "#FF4949";
-            blocks[j + 1].style.backgroundColor = "#FF4949";
-      
-            await new Promise(resolve =>
-              setTimeout(() => {
-                resolve();
-              }, delay)
-            );
-      
-            const value1 = Number(blocks[j].childNodes[0].innerHTML);
-            const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
-      
-            if (value1 > value2) {
-              await swap(blocks[j], blocks[j + 1]);
-              blocks = document.querySelectorAll(".block");
-              var html = "<div class='clear-msg-change-pre' align='center'><p style='width: auto;font-size:42px;color:white;'><b>" + value1 + "⇔" + value2 + "</b></p><div>"; 
-              $('.clear-msg-change-pre').remove();
-              $("#bubble-pre").append(html);
+            if(!paused || i > l_i || (i == l_i && j > l_j)) {
+              blocks[j].style.backgroundColor = "#FF4949";
+              blocks[j + 1].style.backgroundColor = "#FF4949";
+            
+              await new Promise(resolve =>
+                setTimeout(() => {
+                  resolve();
+                }, delay)
+              );
+        
+              const value1 = Number(blocks[j].childNodes[0].innerHTML);
+              const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+              console.log("change" +value1+ " <=> "+value2)
+              if (value1 > value2) {
+                await swap(blocks[j], blocks[j + 1]);
+                blocks = document.querySelectorAll(".block");
+                var html = "<div class='clear-msg-change-pre' align='center'><p style='width: auto;font-size:42px;color:white;'><b>" + value1 + "⇔" + value2 + "</b></p><div>"; 
+                $('.clear-msg-change-pre').remove();
+                $("#bubble-pre").append(html);
+              }
+        
+              blocks[j].style.backgroundColor = "#86ecfb";
+              blocks[j + 1].style.backgroundColor = "#86ecfb";
             }
-      
-            blocks[j].style.backgroundColor = "#86ecfb";
-            blocks[j + 1].style.backgroundColor = "#86ecfb";
         }
       }
       if(!locked) {
